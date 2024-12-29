fd50d9a83c276ad48e628cade6ac6949a60cbc65
suat525255 <suataydin5255@gmail.com>
2024-12-29 13:25:48 +0300

    added hook

---
+++ src/test/java/stepDefinitions/Hooks.java
@@ -0,0 +1,62 @@
+package stepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+import utilities.OS;
+
+
+public class Hooks {
+    private Scenario scenario;
+
+    @Before
+    public void setUp(Scenario scenario) {
+        this.scenario = scenario;
+        OS.OS = ConfigurationReader.getProperty("platformName", "iOS");
+    }
+
+    @Given("LinkedIn uygulamasi acik durumda")
+    public void linkedInUygulamasiAcikDurumda() {
+        try {
+            if (OS.isIOS()) {
+                if (Driver.iOS == null) {
+                    Driver.iOS = Driver.getIOSDriver();
+                }
+            } else if (OS.isAndroid()) {
+                if (Driver.Android == null) {
+                    Driver.Android = Driver.getAndroidDriver();
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Driver başlatma hatası: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        try {
+            WebDriver driver = Driver.getCurrentDriver();
+            if (driver != null && scenario.isFailed()) {
+                if (driver instanceof TakesScreenshot) {
+                    byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                    scenario.attach(screenshot, "image/png", "hata-ekran-goruntusu");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Teardown hatası: " + e.getMessage());
+        } finally {
+            try {
+                Driver.closeDriver();
+            } catch (Exception e) {
+                System.out.println("Driver kapatma hatası: " + e.getMessage());
+            }
+        }
+    }
+}
